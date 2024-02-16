d424525c4eb4879acd3e1e6e94a476cd3f96fe1d
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-02-16 12:27:24 -0300

    𝑴𝒚𝒍𝒊𝒏𝒈𝑩𝒐𝒕-𝑴𝑫

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -13,16 +13,16 @@ bot = global.db.data.settings[this.user.jid] || {}
 
 if (bot.antiPrivate && !isOwner && !isROwner) {
 if (user.counterPrivate === 0) {
-mensaje = `*@${m.sender.split`@`[0]} ¡HOLA! ESTÁ PROHIBIDO ESCRIBIR AL PRIVADO, PORQUE ASÍ LO QUISO MI PROPIETARIO(A).*\n\n⚠️ \`\`\`PRIMERA ADVERTENCIA\`\`\` ⚠️`
+mensaje = `*@${m.sender.split`@`[0]}* 𝙀𝙎𝙏𝘼 𝙋𝙍𝙊𝙃𝙄𝘽𝙄𝘿𝙊 𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝙍 𝘼𝙇 𝙋𝙍𝙄𝙑𝘼𝘿𝙊\n\n⚠️ \`\`\`𝐏𝐑𝐈𝐌𝐄𝐑𝐀 𝐀𝐃𝐕𝐄𝐑𝐓𝐄𝐍𝐂𝐈𝐀\`\`\` ⚠️`
 await conn.reply(m.chat, mensaje, m, { mentions: [m.sender] })  
   
 } else if (user.counterPrivate === 1) {
 let grupos = [ nn, nna2, nnnttt2, nnnttt3, nnnttt4 ].getRandom()
-mensaje = `*@${m.sender.split`@`[0]} YA SE MENCIONÓ QUE NO PUEDE ESCRIBIR AL PRIVADO. 🫤*\n\n👇 *PUEDE UNIRSE A ESTE GRUPO OFICIAL PARA USAR EL BOT*\n${grupos}\n\n*SI VUELVE A ESCRIBIR SERÁ BLOQUEADO(A)* ‼️\n⚠️ \`\`\`SEGUNDA ADVERTENCIA\`\`\` ⚠️`
+mensaje = `*@${m.sender.split`@`[0]}* 𝘼𝙉𝙏𝙀𝙍𝙄𝙊𝙍𝙈𝙀𝙉𝙏𝙀 𝙎𝙀 𝙈𝙀𝙉𝘾𝙄𝙊𝙉𝙊 𝙌𝙐𝙀 𝙉𝙊 𝙎𝙀 𝙋𝙊𝘿𝙄𝘼 𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝘿𝙊 𝘼𝙇 𝙋𝙍𝙄𝙑𝘼𝘿𝙊. 😑\n\n👇 𝙋𝙐𝙀𝘿𝙀 𝙐𝙉𝙄𝙍𝙎𝙀 𝘼 𝙐𝙉 𝙂𝙍𝙐𝙋𝙊 𝙊𝙁𝙄𝘾𝙄𝘼𝙇 𝙋𝘼𝙍𝘼 𝙐𝙎𝘼𝙍 𝙀𝙇 𝘽𝙊𝙏 \n${grupos}\n\n𝙎𝙄 𝙑𝙐𝙀𝙇𝙑𝙀 𝘼 𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝙍 𝙎𝙀𝙍𝘼 𝘽𝙇𝙊𝙌𝙐𝙀𝘼𝘿𝙊(𝘼)‼️\n\n⚠️ \`\`\`𝐒𝐄𝐆𝐔𝐍𝐃𝐀 𝐀𝐃𝐕𝐄𝐑𝐓𝐄𝐍𝐂𝐈𝐀\`\`\`⚠️`
 await conn.reply(m.chat, mensaje, m, { mentions: [m.sender] }) 
   
 } else if (user.counterPrivate === 2) {
-mensaje = `*@${m.sender.split`@`[0]} LAMENTABLEMENTE SERÁ BLOQUEADO(A). 😾 SE MENCIONÓ ANTES QUE NO PODÍA ESCRIBIR AL PRIVADO.*\n\n⚠️ \`\`\`TERCERA ADVERTENCIA\`\`\` ⚠️`
+mensaje = `*@${m.sender.split`@`[0]}* 𝘼𝙃𝙊𝙍𝘼 𝙎𝙀𝙍𝘼 𝘽𝙇𝙊𝙌𝙐𝙀𝘼𝘿𝙊(𝘼).😾🫵 𝙎𝙀 𝙈𝙀𝙉𝘾𝙄𝙊𝙉𝙊 𝘼𝙉𝙏𝙀𝙎 𝙌𝙐𝙀 𝙉𝙊 𝙋𝙊𝘿𝙄𝘼 𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝙍 𝘼𝙇 𝙋𝙍𝙄𝙑𝘼𝘿𝙊\n\n⚠️ \`\`\`𝐓𝐄𝐑𝐂𝐄𝐑𝐀 𝐀𝐃𝐕𝐄𝐑𝐓𝐄𝐍𝐂𝐈𝐀\`\`\` ⚠️`
 await conn.reply(m.chat, mensaje, m, { mentions: [m.sender] }) 
   
 user.counterPrivate = -1
