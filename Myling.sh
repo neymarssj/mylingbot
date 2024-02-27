40bdfcc5d25e458558fdec64c90206a2fc136ccb
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-02-27 20:18:58 -0300

    Update Myling.sh

--- Myling.sh
+++ Myling.sh
@@ -206,9 +206,9 @@ git clone https://github.com/WilsonOFC/MylingBot-MD.git
 echo -e "\033[01;32m\033[01mLa clonación se ha descargado e instalado correctamente.\nThe clone has been downloaded and installed successfully.\n\033[0m"
 
 echo -e "\033[01;32m\033[01mCambiando al directorio del repositorio!!\nChanging to the repository directory!!\n\033[0m" 
-cd KatashiBot-MD
+cd MylingBot-MD
 
-echo -e "\e[36m
+-e "\e[36m
 ██╗░░░██╗██████╗░██████╗░░█████╗░████████╗███████╗  ██╗░░░██╗░█████╗░██████╗░███╗░░██╗
 ██║░░░██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝  ╚██╗░██╔╝██╔══██╗██╔══██╗████╗░██║
 ██║░░░██║██████╔╝██║░░██║███████║░░░██║░░░█████╗░░  ░╚████╔╝░███████║██████╔╝██╔██╗██║
