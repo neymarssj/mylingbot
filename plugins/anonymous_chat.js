da8fd326ba5c0fa59f15a406ffda30354568cc19
WilsonWaoz <135527239+WilsonOFC@users.noreply.github.com>
2024-03-01 08:32:58 -0300

    𝑴𝒚𝒍𝒊𝒏𝒈𝑩𝒐𝒕-𝑴𝑫

--- plugins/anonymous_chat.js
+++ plugins/anonymous_chat.js
@@ -5,26 +5,26 @@ switch (command) {
 case 'next':
 case 'leave': {
 let room = Object.values(this.anonymous).find(room => room.check(m.sender))
-if (!room) return this.sendMessage(m.chat, { text: `${lenguajeGB['smsChatAn1']()}\n${lenguajeGB['smsChatAn2']()}`}, { quoted: m })
-//this.sendButton(m.chat, `${lenguajeGB['smsChatAn1']()}`, lenguajeGB.smsChatAn2() + wm, null, [[lenguajeGB.smsChatAn3(), `.start`]], m)
+if (!room) return this.sendMessage(m.chat, { text: `${lenguajeMY['smsChatAn1']()}\n${lenguajeMY['smsChatAn2']()}`}, { quoted: m })
+//this.sendButton(m.chat, `${lenguajeMY['smsChatAn1']()}`, lenguajeMY.smsChatAn2() + wm, null, [[lenguajeMY.smsChatAn3(), `.start`]], m)
 m.reply(`${lenguajeGB['smsChatAn4']()}`)
 let other = room.other(m.sender) 
-if (other) await this.sendMessage(other, { text: `${lenguajeGB['smsChatAn5']()}`}, { quoted: m })
-//this.sendButton(other, `${lenguajeGB['smsChatAn5']()}`, lenguajeGB.smsChatAn6() + wm, null, [[lenguajeGB.smsChatAn3(), `.start`]], m)
+if (other) await this.sendMessage(other, { text: `${lenguajeMY['smsChatAn5']()}`}, { quoted: m })
+//this.sendButton(other, `${lenguajeMY['smsChatAn5']()}`, lenguajeMY.smsChatAn6() + wm, null, [[lenguajeMY.smsChatAn3(), `.start`]], m)
 delete this.anonymous[room.id]
 if (command === 'leave') break
 }
 case 'start': {
-if (Object.values(this.anonymous).find(room => room.check(m.sender))) return this.sendMessage(m.chat, { text: `${lenguajeGB['smsChatAn7']()}`}, { quoted: m })
-//this.sendButton(m.chat, `${lenguajeGB['smsChatAn7']()}`, lenguajeGB.smsChatAn8() + wm, null, [[lenguajeGB.smsChatAn9(), `.leave`]], m)
+if (Object.values(this.anonymous).find(room => room.check(m.sender))) return this.sendMessage(m.chat, { text: `${lenguajeMY['smsChatAn7']()}`}, { quoted: m })
+//this.sendButton(m.chat, `${lenguajeMY['smsChatAn7']()}`, lenguajeMY.smsChatAn8() + wm, null, [[lenguajeMY.smsChatAn9(), `.leave`]], m)
 let room = Object.values(this.anonymous).find(room => room.state === 'WAITING' && !room.check(m.sender))
 if (room) {
-await this.sendMessage(room.a, { text: `${lenguajeGB['smsChatAn10']()}`}, { quoted: m })
-//this.sendButton(room.a, `${lenguajeGB['smsChatAn10']()}`, lenguajeGB.smsChatAn11() + wm, null, [[lenguajeGB.smsChatAn12(), `.next`]], m)
+await this.sendMessage(room.a, { text: `${lenguajeMY['smsChatAn10']()}`}, { quoted: m })
+//this.sendButton(room.a, `${lenguajeMY['smsChatAn10']()}`, lenguajeMY.smsChatAn11() + wm, null, [[lenguajeMY.smsChatAn12(), `.next`]], m)
 room.b = m.sender
 room.state = 'CHATTING'
-await this.sendMessage(m.chat, { text: `${lenguajeGB['smsChatAn10']()}`}, { quoted: m })
-//this.sendButton(m.chat, `${lenguajeGB['smsChatAn10']()}`, lenguajeGB.smsChatAn11() + wm, null, [[lenguajeGB.smsChatAn12(), `.next`]], m)
+await this.sendMessage(m.chat, { text: `${lenguajeMY['smsChatAn10']()}`}, { quoted: m })
+//this.sendButton(m.chat, `${lenguajeMY['smsChatAn10']()}`, lenguajeMY.smsChatAn11() + wm, null, [[lenguajeMY.smsChatAn12(), `.next`]], m)
 } else {
 let id = + new Date
 this.anonymous[id] = {
@@ -39,8 +39,8 @@ other: function (who = '') {
 return who === this.a ? this.b : who === this.b ? this.a : ''
 },
 }
-await this.sendMessage(m.chat, { text: `${lenguajeGB['smsChatAn13']()}`}, { quoted: m })
-//this.sendButton(m.chat, `${lenguajeGB['smsChatAn13']()}`, lenguajeGB.smsChatAn8() + wm, null, [[lenguajeGB.smsChatAn9(), `.leave`]], m)
+await this.sendMessage(m.chat, { text: `${lenguajeMY['smsChatAn13']()}`}, { quoted: m })
+//this.sendButton(m.chat, `${lenguajeMY['smsChatAn13']()}`, lenguajeMY.smsChatAn8() + wm, null, [[lenguajeMY.smsChatAn9(), `.leave`]], m)
 }
 break
 }}}
