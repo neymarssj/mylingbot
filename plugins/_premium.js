2c947315b89969b11e857316e025eae62a4f08db
WilsonWaoz <135527239+WilsonOFC@users.noreply.github.com>
2024-03-01 08:12:47 -0300

    𝑴𝒚𝒍𝒊𝒏𝒈𝑩𝒐𝒕-𝑴𝑫

--- plugins/_premium.js
+++ plugins/_premium.js
@@ -4,7 +4,7 @@ let user = global.db.data.users[m.sender]
 if (m.chat.endsWith('broadcast')) return
 if (user.premiumTime != 0 && user.premium) {
 if (new Date() * 1 >= user.premiumTime) {
-await m.reply(`${lenguajeGB['smsAvisoAG']()}*@${m.sender.split`@`[0]}* ${lenguajeGB['smsPremI']()}`, false, { mentions: [m.sender] })
+await m.reply(`${lenguajeMY['smsAvisoAG']()}*@${m.sender.split`@`[0]}* ${lenguajeMY['smsPremI']()}`, false, { mentions: [m.sender] })
 user.premiumTime = 0
 user.premium = false
 }}}
