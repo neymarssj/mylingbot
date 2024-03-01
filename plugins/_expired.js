ff1e0dae5c70ae44d4d47dbe6dfb7822c79bad3c
WilsonWaoz <135527239+WilsonOFC@users.noreply.github.com>
2024-03-01 08:11:29 -0300

    𝑴𝒚𝒍𝒊𝒏𝒈𝑩𝒐𝒕-𝑴𝑫

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -6,11 +6,11 @@ let chats = global.db.data.chats[m.chat]
 if (!chats.expired)
 return !0
 if (+new Date() > chats.expired) {
-//let caption = `*${this.user.name}* ${lenguajeGB['smsBottem1']()}`
+//let caption = `*${this.user.name}* ${lenguajeMY['smsBottem1']()}`
 let pp = './media/menus/Menu2.jpg'
 
-await m.reply(`${this.user.name}\n ${lenguajeGB['smsBottem1']()}`)
-//await this.sendButton(m.chat, caption, lenguajeGB.smsBottem2() + wm, pp, [[lenguajeGB.smsBottem3(), '.hastapronto']], null)
+await m.reply(`${this.user.name}\n ${lenguajeMY['smsBottem1']()}`)
+//await this.sendButton(m.chat, caption, lenguajeMY.smsBottem2() + wm, pp, [[lenguajeMY.smsBottem3(), '.hastapronto']], null)
 await this.groupLeave(m.chat)
 chats.expired = null
 }}
