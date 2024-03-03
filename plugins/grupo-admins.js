6b9252521e63a9dd918de7d7ac0f568680d951b1
WilsonWaoz <135527239+WilsonOFC@users.noreply.github.com>
2024-03-03 03:51:24 -0300

    𝑴𝒚𝒍𝒊𝒏𝒈𝑩𝒐𝒕-𝑴𝑫

--- plugins/grupo-admins.js
+++ plugins/grupo-admins.js
@@ -1,22 +1,22 @@
 let handler = async (m, { conn, participants, groupMetadata, args, usedPrefix, text, command }) => {
-  if (!text) return m.reply(`${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊 𝙋𝙊𝙍 𝘾𝙐𝘼𝙇 𝙌𝙐𝙄𝙀𝙍𝙀 𝙎𝙊𝙇𝙄𝘾𝙄𝙏𝘼 𝙇𝘼 𝙋𝙍𝙀𝙎𝙀𝙉𝘾𝙄𝘼 𝘿𝙀𝙇 𝙇𝙊𝙎 𝘼𝘿𝙈𝙄𝙉𝙎`)
+  if (!text) return m.reply(`${lenguajeMY['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙏𝙀𝙓𝙏𝙊 𝙋𝙊𝙍 𝘾𝙐𝘼𝙇 𝙌𝙐𝙄𝙀𝙍𝙀 𝙎𝙊𝙇𝙄𝘾𝙄𝙏𝘼 𝙇𝘼 𝙋𝙍𝙀𝙎𝙀𝙉𝘾𝙄𝘼 𝘿𝙀𝙇 𝙇𝙊𝙎 𝘼𝘿𝙈𝙄𝙉𝙎`)
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/admins.jpg'
 const groupAdmins = participants.filter(p => p.admin)
 const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 let pesan = args.join` `
-let oi = `${lenguajeGB.smsAddB5()} _${pesan}_`
+let oi = `${lenguajeMY.smsAddB5()} _${pesan}_`
 
 let textoA = 
 `*⊱ ──── 《.⋅ 🍁 ⋅.》 ──── ⊰*
-ෆ ${lenguajeGB.smsAddB3()}
+ෆ ${lenguajeMY.smsAddB3()}
 ෆ ${oi}
 *⊱ ──── 《.⋅ ${vs} ⋅.》 ──── ⊰*`
 
 let textoB = 
 `${listAdmin}
 
-⛔ ${lenguajeGB.smsAddB4()} ⛔`.trim()
+⛔ ${lenguajeMY.smsAddB4()} ⛔`.trim()
 await conn.sendFile(m.chat, pp, 'error.jpg', textoA + textoB, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
 //await conn.sendButton(m.chat, textoA, textoB, pp, [[lenguajeGB.smsConMenu(), `.menu`]], m, { mentions: [...groupAdmins.map(v => v.id), owner] })
 }
