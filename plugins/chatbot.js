d926ad303654fdf4f78961f2ae453cd72de4025c
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-03-13 21:35:37 -0300

    𝑴𝒚𝒍𝒊𝒏𝒈𝑩𝒐𝒕-𝑴𝑫

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -1,6 +1,4 @@
-By: elrebelde21
- 
-/*let handler = m => m
+let handler = m => m
 handler.all = async function (m) {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let chat = global.db.data.chats[m.chat]
@@ -16,41 +14,39 @@ conn.reply(m.chat, teks, m, { mentions: { mentionedJid: [m.sender] }})
 
 //if (/^Mande porno|porno|paja$/i.test(m.text) ) { //sin prefijo 
     let teks = `
-${pickRandom([`no puedo esta contra las política del grupo.👻`, `_uff miren un pajero_`, `_pagame y paso mi pack😏🥵_`, `_que_`, `_que quiere pija dice 🤣_`, `_pasa el pack de tu hermana😏_`, `_mire un gilipolla_`, `_siuuu sexo sexo sexo😈_`, '_callate putito_'])}
+${pickRandom([`no puedo esta contra las política del grupo. 👻`, `_uff miren un pajero_`, `_pagame y paso mi pack😏🥵_`, `_que_`, `_que quiere pija dice 🤣_`, `_pasa el pack de tu hermana😏_`, `_pasas una fototeta? 🤨📸_`, `_siuuu sexo sexo sexo😈_`, '_callate negro no tiene derecho XD_'])}
 `.trim()
 conn.reply(m.chat, teks, m, { mentions: { mentionedJid: [m.sender] }})
 
 }
 
 if (/^reglas|normas|Reglas$/i.test(m.text) ) { //sin prefijo 
-    conn.reply(m.chat, `╭┅〘 ⚠️ 𝗢𝗯𝗲𝗱𝗲𝗰𝗲 𝗹𝗮𝘀 𝗿𝗲𝗴𝗹𝗮𝘀 ⚠️ 〙*
-➽❌ 𝐏𝐫𝐨𝐡𝐢𝐛𝐢𝐝𝐨 𝐥𝐥𝐚𝐦𝐚𝐫 𝐚𝐥 𝐁𝐨𝐭
-➽❌ 𝐏𝐫𝐨𝐡𝐢𝐛𝐢𝐝𝐨 𝐒𝐩𝐚𝐦 𝐚𝐥 𝐁𝐨𝐭
-➽❌ 𝐍𝐨 𝐚𝐠𝐫𝐞𝐠𝐚𝐫 𝐚𝐥 𝐁𝐨𝐭
-➽❌ 𝐑𝐞𝐬𝐩𝐞𝐭𝐚 𝐥𝐨𝐬 𝐭𝐞𝐫𝐦𝐢𝐧𝐨𝐬 𝐲 𝐜𝐨𝐧𝐝𝐢𝐜𝐢𝐨𝐧𝐞𝐬
-*╰═┅ৡৢ͜͡✦═╡  ⓀⒶⓉⒶⓈⒽⒾ ⒷⓄⓉ ╞═┅ৡৢ͜͡✦═╯*`, fkontak, m) //wm, null, [['Menu', '#menu']], m) botones :V
+    conn.reply(m.chat, `┏━🜲 ⚠️ 𝐒𝐈𝐆𝐔𝐄 𝐋𝐀𝐒 𝐑𝐄𝐆𝐋𝐀𝐒 ⚠️ 🜲━┓*
+❌ 𝐏𝐑𝐎𝐇𝐈𝐁𝐈𝐃𝐎 𝐋𝐋𝐀𝐌𝐀𝐑 𝐀𝐋 𝐁𝐎𝐓
+❌ 𝐏𝐑𝐎𝐇𝐈𝐁𝐈𝐃𝐎 𝐄𝐋 𝐒𝐏𝐀𝐌 𝐀𝐋 𝐁𝐎𝐓
+❌ 𝐍𝐎 𝐀𝐆𝐑𝐄𝐆𝐀𝐑 𝐀𝐋 𝐁𝐎𝐓 𝐀 𝐆𝐑𝐔𝐏𝐎𝐒
+❌ 𝐑𝐄𝐒𝐏𝐄𝐓𝐀𝐑 𝐋𝐎𝐒 𝐓𝐄𝐑𝐌𝐈𝐍𝐎𝐒 𝐘 𝐂𝐎𝐍𝐃𝐈𝐂𝐈𝐎𝐍𝐄𝐒 𝐃𝐄𝐋 𝐁𝐎𝐓
+*≫ 𝐌𝐘𝐋𝐈𝐍𝐆-𝐁𝐎𝐓-𝐌𝐃 ≪*`, fkontak, m) //wm, null, [['Menu', '#menu']], m) botones :V
 
 }
 
        if (/^hola|halo|hello|hi$/i.test(m.text) ) { //sin prefijo 
-     conn.reply(m.chat, `Hola :D`, fkontak, m)
+     conn.reply(m.chat, `HOLA BRO :D`, fkontak, m)
 
  }
  
 if (/^¿que es un bot?|Que es un bot|Que es MylingBot?$/i.test(m.text) ) { //sem prefixo
     conn.reply(m.chat, `╭┄〔 *${wm}* 〕┄⊱
 ┆ ——————«•»——————
-┆ ☆::¿𝐐𝐮𝐞́ 𝐞𝐬 𝐮𝐧 𝐁𝐨𝐭 𝐝𝐞 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩?::☆
+┆ ☆:: ¿𝐐𝐔𝐄 𝐄𝐒 𝐔𝐍 𝐁𝐎𝐓 𝐃𝐄 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏? ::☆
 ┆——————«•»——————
-┆ 𝐔𝐧 𝐁𝐨𝐭 𝐞𝐬 𝐮𝐧𝐚 𝐢𝐧𝐭𝐞𝐥𝐢𝐠𝐞𝐧𝐜𝐢𝐚 𝐚𝐫𝐭𝐢𝐟𝐢𝐜𝐢𝐚𝐥 𝐪𝐮𝐞 𝐫𝐞𝐚𝐥𝐢𝐳𝐚 𝐭𝐚𝐫𝐞𝐚𝐬
-┆ 𝐪𝐮𝐞 𝐥𝐞 𝐢𝐧𝐝𝐢𝐪𝐮𝐞 𝐜𝐨𝐧 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬, 𝐞𝐧 𝐞𝐥 𝐜𝐚𝐬𝐨 𝐝𝐞 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 
-┆ 𝐩𝐮𝐞𝐝𝐞𝐬 𝐜𝐫𝐞𝐚𝐫 𝐬𝐭𝐢𝐜𝐤𝐞𝐫𝐬, 𝐝𝐞𝐬𝐜𝐚𝐫𝐠𝐚𝐫 𝐦𝐮́𝐬𝐢𝐜𝐚, 𝐯𝐢𝐝𝐞𝐨𝐬, 
-┆ 𝐜𝐫𝐞𝐚𝐫 𝐥𝐨𝐠𝐨𝐬 𝐩𝐞𝐫𝐬𝐨𝐧𝐚𝐥𝐢𝐳𝐚𝐝𝐨𝐬 𝐲 𝐦𝐮𝐜𝐡𝐨 𝐦𝐚𝐬, 
-┆ 𝐞𝐬𝐭𝐨 𝐝𝐞 𝐟𝐨𝐫𝐦𝐚 𝐚𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐳𝐚𝐝𝐚, 𝐨 𝐬𝐞𝐚 𝐪𝐮𝐞 𝐮𝐧 𝐡𝐮𝐦𝐚𝐧𝐨 
-┆ 𝐧𝐨 𝐢𝐧𝐭𝐞𝐫𝐟𝐢𝐞𝐫𝐞 𝐞𝐧 𝐞𝐥 𝐩𝐫𝐨𝐜𝐞𝐬𝐨 
-┆ 𝐏𝐚𝐫𝐚 𝐯𝐞𝐫 𝐞𝐥 𝐦𝐞𝐧𝐮́ 𝐝𝐞 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 𝐩𝐮𝐞𝐝𝐞𝐬 𝐮𝐬𝐚𝐫 #menu
+┆UN BOT DE WHATSAPP O UN BOT ES UN PROGRAMA DE SOFTWARE AVANZADO CREADO POR CODIGOS DE PROGRAMACION, ESTOS SON CREADOS POR DIFERENTES DESARROLLADORES Y DIFERENTES MOTIVOS O OBJECTIVOS A REALIZARSE.
+┊
+┊LOS BOTS EN ESTE CASO DE WHATSAPP CUMPLEN FUNCIONALIDADES UNICAS Y MEJORADAS.
+┊
+┊LOS BOTS DE WHATSAPP SIRVEN O SE UTILIZA PARA DESCARGAR TODO TIPO DE CONTENIDO EN REDES SOCIALES COMO ARCHIVOS/AUDIOS/IMAGENES O MULTIMEDIA, ADEMAS SON UTLIZABLES TANTO EN GRUPOS COMO PRIVADO TAMBIEN SIRVEN PARA HACER GESTIONES DE TUS GRUPOS COMO ELIMINAR O AGREGAR USUARIOS O SILENCIARLOS Y MUCHO MAS.
 ┆ 
-┆ 「 ⓀⒶⓉⒶⓈⒽⒾ ⒷⓄⓉ 」
+┆ 「 ⓂⓎⓁⒾⓃⒼ-ⒷⓄⓉ-ⓂⒹ 」
 ╰━━━⊰ 𓃠 ${vs} ⊱━━━━დ*`, m) //wm, null, [['Menu', '#menu']], m) botones :V
 
 }  
