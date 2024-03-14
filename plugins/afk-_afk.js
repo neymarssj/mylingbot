2d98bc7556124d3f6d2990d3004be5412bff1a77
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-03-13 21:09:27 -0300

    𝑴𝒚𝒍𝒊𝒏𝒈𝑩𝒐𝒕-𝑴𝑫

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -2,11 +2,11 @@ let handler = m => m
 handler.before = async function (m, { text, args, usedPrefix, command, conn } ) {
 let user = global.db.data.users[m.sender]
 if (user.afk > -1) {
-await conn.reply(m.chat, `${lenguajeGB['smsAvisoEG']()}❗ *A F K* ❗
+await conn.reply(m.chat, `${lenguajeMY['smsAvisoEG']()}❗ *A F K* ❗
 *▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔*
-*@${m.sender.split("@")[0]}* ${lenguajeGB['smsAfkM1']()}${user.afkReason ? `\n${lenguajeGB['smsAfkM2']()}👉 ` + user.afkReason : ''}
+*@${m.sender.split("@")[0]}* ${lenguajeMY['smsAfkM1']()}${user.afkReason ? `\n${lenguajeMY['smsAfkM2']()}👉 ` + user.afkReason : ''}
 
-${lenguajeGB['smsAfkM3']()}\n👉 *${(new Date - user.afk).toTimeString()}*`.trim(), m, { mentions: [m.sender] })
+${lenguajeMY['smsAfkM3']()}\n👉 *${(new Date - user.afk).toTimeString()}*`.trim(), m, { mentions: [m.sender] })
 user.afk = -1
 user.afkReason = ''
 }
@@ -19,11 +19,11 @@ let afkTime = user.afk
 if (!afkTime || afkTime < 0)
 continue
 let reason = user.afkReason || ''
-await conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}❕ *A F K* ❕
+await conn.reply(m.chat, `${lenguajeMY['smsAvisoAG']()}❕ *A F K* ❕
 *▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔*
-😾 ${lenguajeGB['smsAfkM4']()}\n${reason ? `${lenguajeGB['smsAfkM5']()}` + '👉 ' + reason : `${lenguajeGB['smsAfkM6']()}`}
+😾 ${lenguajeMY['smsAfkM4']()}\n${reason ? `${lenguajeMY['smsAfkM5']()}` + '👉 ' + reason : `${lenguajeMY['smsAfkM6']()}`}
 
-${lenguajeGB['smsAfkM3']()}\n👉 *${(new Date - user.afk).toTimeString()}*`.trim(), m)
+${lenguajeMY['smsAfkM3']()}\n👉 *${(new Date - user.afk).toTimeString()}*`.trim(), m)
 }
 return true
 }
