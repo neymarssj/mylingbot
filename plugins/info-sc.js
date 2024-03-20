cfa9ba9ff8257ef711d62272ac694aa72f1739e1
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-03-20 17:39:17 -0300

    𝑴𝒚𝒍𝒊𝒏𝒈𝑩𝒐𝒕-𝑴𝑫

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -16,7 +16,7 @@ function kyun(seconds){
   var seconds = Math.floor(seconds % 60);
 
   //return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds)
-  return `🌟  𝘽 𝙊 𝙏  -  𝙎 𝘾 𝙍 𝙄 𝙋 𝙏  🌟\n\n*» 𝙉𝙊𝙈𝘽𝙍𝙀:* ${json?.name || '𝖬𝖸𝖫𝖨𝖭𝖦𝖡𝖮𝖳-𝖬𝖣'}\n*» 𝙑𝙄𝙎𝙄𝙏𝘼𝙉𝙏𝙀𝙎:* ${json?.watchers_count || '-'}\n*» 𝘼𝘾𝙏𝙐𝘼𝙇𝙄𝙕𝘼𝘾𝙄𝙊𝙉:* ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || '-'}\n*» 𝙐𝙍𝙇:* ${json?.html_url || 'https://github.com/WilsonOFC/MylingBot-MD'}\n\n${json?.forks_count || '-'} 𝖥𝖮𝖱𝖪𝖲 - 𝖥𝖮𝖱𝖪𝖲 · ${json?.stargazers_count || '-'} 𝖲𝖳𝖠𝖱𝖲 - 𝖤𝖲𝖳𝖱𝖤𝖫𝖫𝖠𝖲 · ${json?.open_issues_count || '-'} 𝖨𝖲𝖲𝖴𝖤𝖲 - 𝖨𝖲𝖲𝖴𝖤𝖲 \n\n ⏰ 𝙏𝙄𝙀𝙈𝙋𝙊 𝘼𝘾𝙏𝙄𝙑𝙊 | 𝘼𝘾𝙏𝙄𝙑𝙀 𝙏𝙄𝙈𝙀:\n \t${pad(days)} Dias\t ${pad(hours)} Horas ${pad(minutes)} Minutos ${pad(seconds)} Segudos \t\n`
+  return `👻  𝘽 𝙊 𝙏  -  𝙎 𝘾 𝙍 𝙄 𝙋 𝙏  👻\n\n*» 𝙉𝙊𝙈𝘽𝙍𝙀:* ${json?.name || '𝖬𝖸𝖫𝖨𝖭𝖦𝖡𝖮𝖳-𝖬𝖣'}\n*» 𝙑𝙄𝙎𝙄𝙏𝘼𝙉𝙏𝙀𝙎:* ${json?.watchers_count || '-'}\n*» 𝘼𝘾𝙏𝙐𝘼𝙇𝙄𝙕𝘼𝘾𝙄𝙊𝙉:* ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || '-'}\n*» 𝙐𝙍𝙇:* ${json?.html_url || 'https://github.com/WilsonOFC/MylingBot-MD'}\n\n${json?.forks_count || '-'} 𝖥𝖮𝖱𝖪𝖲 · ${json?.stargazers_count || '-'} 𝖲𝖳𝖠𝖱𝖲 · ${json?.open_issues_count || '-'} 𝖨𝖲𝖲𝖴𝖤𝖲 \n\n 彡 𝙏𝙄𝙀𝙈𝙋𝙊 𝘼𝘾𝙏𝙄𝙑𝙊 | 𝘼𝘾𝙏𝙄𝙑𝙀 𝙏𝙄𝙈𝙀:\n \t${pad(days)} Dias\t ${pad(hours)} Horas ${pad(minutes)} Minutos ${pad(seconds)} Segudos \t\n`
 }
 					const runtime = process.uptime()
 		            const teks = `${kyun(runtime)}`
