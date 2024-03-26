a0d7714712185f2f8fe4d8aa315a0b86983249b7
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-26 18:54:05 -0500

    Actualizar main.js

--- main.js
+++ main.js
@@ -167,7 +167,7 @@ const connectionOptions = {
   logger: pino({ level: 'silent' }),
   printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
   mobile: MethodMobile, 
-  browser: opcion == '1' ? ['MylingBot-MD', 'Edge', '2.0.0'] : methodCodeQR ? ['MylingBot-MD', 'Edge', '2.0.0'] : ['Chrome (Linux)', '', ''],
+  browser: opcion == '1' ? ['MylingBot-MD', 'Edge', '2.0.0'] : methodCodeQR ? ['MylingBot-MD', 'Edge', '2.0.0'] : ['Ubuntu', 'Chrome', '110.0.5585.95'],
   auth: {
   creds: state.creds,
   keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
