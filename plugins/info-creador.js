e04fe24a9be576aa7829ad725074244128fce676
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-03-27 02:20:09 -0300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -2,15 +2,22 @@ var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let pp = gataImg.getRandom()	
 const cat = `
-彡 ⪻ 𝐌𝐲𝐥𝐢𝐧𝐠𝐁𝐨𝐭-𝐌𝐃 ⪼ 彡
+彡-[𝐌𝐲𝐥𝐢𝐧𝐠𝐁𝐨𝐭-𝐌𝐃 彡
+<ᴄᴏɴᴛᴀᴄᴛᴀ ᴀ ᴍɪ ᴄʀᴇᴀᴅᴏʀ>
+- ɴᴜᴍᴇʀᴏ ᴅᴇ ᴍɪ ᴄʀᴇᴀᴅᴏʀ
 
-«───» 𝐂𝐑𝐄𝐀𝐃𝐎𝐑 «───»
-- 𝐖𝐈𝐋𝐒𝐎𝐍𝐎𝐅𝐂 - 𝐖𝐈𝐋𝐒𝐎𝐍
 - ${asistencia}
 
-- _CONTACTE A MI CREADOR PARA CUALQUIER CONSULTA O PROBLEMA QUE TENGA CON EL BOT._
+- *¿TIENES DUDAS DEL BOT?*
 
-«─────────────»
+- *¿TIENES ALGUNA QUEJA CON EL BOT?*
+
+- *¿TE GUSTARIA COMPARTIR OPINIONES, SUGERENCIAS DEL BOT?*
+
+*¡PERFECTO!*
+
+> CONTACTA CON MI CREADOR PARA CUALQUIER TEMA DEL BOT, NO ESCRIBIR SI NO ES POR TEMA DEL BOT, EVITE PROBLEMAS.
+(☽︎─────────────☾)
 `
 
 await conn.sendFile(m.chat, pp, 'Hola.mp3', cat, fkontak)
