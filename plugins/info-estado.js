fad446cc1ab8e120415e707b52f75e11aec2338f
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-03-27 02:58:37 -0300

    Update info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -8,7 +8,7 @@ _muptime = await new Promise(resolve => { process.once('message', resolve)
 setTimeout(resolve, 1000) }) * 1000}
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let uptime = clockString(_uptime)
-let estado = `★,｡･::･ﾟ☆[ *ESTADO* ] ☆･::･ﾟ,｡★
+let estado = `★,｡･::･ﾟ☆[ *ESTADO* ] ☆ﾟ･::･,｡★
 ❥︎ *¡Hola!* ${name}
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈ 
 ❥︎ *👻 MʏʟɪɴɢBᴏᴛ-MD 👻*
