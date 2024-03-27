dbbe11d5ce3356d006ba366288f750f1081a3c29
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-03-27 02:32:06 -0300

    Update info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -8,19 +8,19 @@ _muptime = await new Promise(resolve => { process.once('message', resolve)
 setTimeout(resolve, 1000) }) * 1000}
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let uptime = clockString(_uptime)
-let estado = `╭── ✦ ──『 𝙀𝙎𝙏𝘼𝘿𝙊 』── ✦ ── ❖ ──╗
-┃✧ *¡Hola!* ${name}
+let estado = `╭── ✿︎ ──[ *ESTADO* ]── ❣✿︎ ── ✿︎ ──╗
+❥︎ *¡Hola!* ${name}
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈ 
-┃ *👻 MylingBot-MD 👻*
+❥︎ *👻 MʏʟɪɴɢBᴏᴛ-MD 👻*
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ *ACTIVO DURANTE:* ${uptime}
+❥︎ *Aᴄᴛɪᴠᴏ ᴅᴜʀᴀɴᴛᴇ:* ${uptime}
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ *USUARIOS:* ${Object.keys(global.db.data.users).length} 
+❥︎ *ᴜsᴜᴀʀɪᴏs:* ${Object.keys(global.db.data.users).length} 
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ *CHATS PROHIBIDOS:* ${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length} 
+❥︎ *Cʜᴀᴛs ʙʟᴏǫᴜᴇᴀᴅᴏs/ʙᴀɴᴇᴀᴅᴏs:* ${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length} 
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃ *USUARIOS PROHIBIDOS:* ${Object.entries(global.db.data.users).filter(user => user[1].banned).length} 
-╰─ ❖ ── ✦ ── ✧ ── ✦ ── ❖ ──╝`
+┃❥︎ *Usᴜᴀʀɪᴏs ʙᴀɴᴇᴀᴅᴏs:* ${Object.entries(global.db.data.users).filter(user => user[1].banned).length} 
+╰─ ❣︎ ── ❣︎ ── ❣︎ ── ❣︎ ── ❣︎ ──╝`
 await conn.sendFile(m.chat, picture, 'Hola.mp3', estado, fkontak) // TESTEO CON ESTO DESPUES /*false, { contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: ds, body: ' 𝑴𝒚𝒍𝒊𝒏𝒈𝑩𝒐𝒕-𝑴𝑫 ', previewType: 0, thumbnail: tagmd, sourceUrl: accountsmy }}})*/
 }
 handler.help = ['estado']
