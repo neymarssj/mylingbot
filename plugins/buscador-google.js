ecf902b61c09cb7c22764522d20927e51b730de6
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-03-27 01:35:53 -0300

    Update buscador-google.js

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -5,11 +5,11 @@ let handler = async (m, { conn, command, args, usedPrefix }) => {
 const fetch = (await import('node-fetch')).default;
 const text = args.join` `;
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-if (!text) return conn.reply(m.chat, `${lenguajeMY['smsAvisoMG']()}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙇𝙊 𝙌𝙐𝙀 𝙌𝙐𝙄𝙀𝙍𝙀 𝘽𝙐𝙎𝘾𝘼𝙍\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} Fantasmas*\n\n𝙏𝙔𝙋𝙀 𝙒𝙃𝘼𝙏 𝙔𝙊𝙐 𝙒𝘼𝙉𝙏 𝙏𝙊 𝙎𝙀𝘼𝙍𝘾𝙃 𝙁𝙊𝙍\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} Ghost*`, m)
+if (!text) return conn.reply(m.chat, `${lenguajeMY['smsAvisoMG']()}Hᴏʟᴀ, ᴘᴀʀᴀ ᴜsᴏ ᴇʀʀᴏɴᴇᴏ ᴅᴇʟ ᴄᴏᴍᴀɴᴅᴏ \nEᴊᴇᴍᴘʟᴏs:\n*${usedPrefix + command} Mᴇssɪ*\n\nTʀᴀᴛᴀ ᴅᴇ ʙᴜsᴄᴀʀ ᴄᴏsᴀs ᴠᴀʟɪᴅᴀs\nOᴛʀᴏ ᴇᴊᴇᴍᴘʟᴏ:\n*${usedPrefix + command} CR7*`, m)
 try {
 const url = 'https://google.com/search?q=' + encodeURIComponent(text);
 google({'query': text}).then(res => {
-let teks = `*🔍 RESULTADOS DE:* ${text}\n\n*${url}*\n\n`
+let teks = `*💻 Rᴇsᴜʟᴛᴀᴅᴏs ᴅᴇ:* ${text}\n\n*${url}*\n\n`
 for (let g of res) {
 teks += `_${g.title}_\n_${g.link}_\n_${g.snippet}_\n\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n\n`
 } 
