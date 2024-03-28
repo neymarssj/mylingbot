b9907806e3c245249b0d82dcf7c847d3fd705636
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-03-28 14:28:08 -0300

    𝑴𝒚𝒍𝒊𝒏𝒈𝑩𝒐𝒕-𝑴𝑫

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -5,7 +5,7 @@ handler.all = async function (m, {conn}) {
 let chat = global.db.data.chats[m.chat]
     
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
-let stiker = await sticker(imagen13, false, global.packname, global.author)  
+let stiker = await sticker(imagen1, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
 contextInfo: { externalAdReply: { title: '⋉ 𝐌𝐲𝐥𝐢𝐧𝐠𝐁𝐨𝐭-𝐌𝐃 ⋊', body: '⪩ CREATE BY WILSON', sourceUrl: `https://github.com/WilsonOFC/MylingBot-MD`, thumbnail: imagen2}}})}
     
