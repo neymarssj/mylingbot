0ec088358e748f097d1d4ab6dea1703c870cc43d
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-03-28 12:59:51 -0500

    Actualizar _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -5,9 +5,9 @@ handler.all = async function (m, {conn}) {
 let chat = global.db.data.chats[m.chat]
 
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
-let stiker = await sticker(gataImg, false, global.packname, global.author)  
+let stiker = await sticker(imagen1, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: '☆𝑴𝒚𝒍𝒊𝒏𝒈𝑩𝒐𝒕-𝑴𝑫☆', body: '𝖡𝗒: 𝖶𝗂𝗅𝗌𝗈𝗇 - 𝖶𝖺𝗈𝗌', sourceUrl: `https://github.com/WilsonOFC/MylingBot-MD/`, thumbnail: gataImg}}})}
+contextInfo: { externalAdReply: { title: '☆𝑴𝒚𝒍𝒊𝒏𝒈𝑩𝒐𝒕-𝑴𝑫☆', body: '𝖡𝗒: 𝖶𝗂𝗅𝗌𝗈𝗇 - 𝖶𝖺𝗈𝗌', sourceUrl: `https://github.com/WilsonOFC/MylingBot-MD/`, thumbnail: imagen2}}})}
 
 return !0 }
 export default handler
