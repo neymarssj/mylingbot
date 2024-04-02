fdbabe64192fe1c0986711c7e5e52f2e177bc306
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-04-01 21:03:32 -0300

    𝑴𝒚𝒍𝒊𝒏𝒈𝑩𝒐𝒕-𝑴𝑫

--- plugins/info-firstchat.js
+++ plugins/info-firstchat.js
@@ -1,19 +1,22 @@
-/*export async function before(m) {
-
+export async function before(m) {
+  
 const username = conn.getName(m.sender)
 
+{'key': {'participants': '0@s.whatsapp.net', 'fromMe': false, 'id': '3B64558B07848BD81108C1D14712018E'}, 'message': {'locationMessage': {'name': `${user}`, 'jpegThumbnail': await (await fetch(pp)).buffer(), 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'}
+  
 if (m.chat.endsWith('broadcast') || m.fromMe || m.isGroup) return
   
 let user = global.db.data.users[m.sender]
 
 if (new Date() - user.pc < 21600000) return
-await m.reply(`🌟 *Hola ${username}, bienvenido a KatashiBot* 🥷🏻
-📲 _Ejecute el comando .menu para visualizar los comandos que dispongo_ 🫡
+await m.reply(`_✨ BIENVENIDO ✨_ 
 
-📌 *Cualquier duda o sugerencia puede contactar a mi creador:* 🎉
-📩 wa.me/51948705559 📭
-🙏❣️ *Si desea apoyar el proyecto Katashi Bot puede donar a esta cuenta por PayPal* ✅📈
-🌟 https://www.paypal.com/paypalme/KatashiF ✨`) 
+_SOY MYLINGBOT-MD DISFRUTA DE MIS FUNCIONES Y COMANDOS 👻_
+
+_🌹 RECUERDATE SEGUIRME EN MI CANAL DE WHATSAPP PARA RECIBIR TODAS LAS ACTUALIZACIONES E INFORMACIONES_
+*${cnl}*
+
+_ESCRIBE EL COMANDO /MENU PARA VER TODAS MIS FUNCIONES Y UN MENU HERMOSO 💖_
+`) 
 user.pc = new Date * 1
 }
-*/
