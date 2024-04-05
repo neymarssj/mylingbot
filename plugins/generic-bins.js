5abb911f9af207b4ed5242dcd6da143d3af1d289
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-04-04 22:34:57 -0300

    𝑴𝒚𝒍𝒊𝒏𝒈𝑩𝒐𝒕-𝑴𝑫

---
+++ plugins/generic-bins.js
@@ -0,0 +1,23 @@
+let handler  = async (m, { conn, usedPrefix, command }) => {
+const fkontak = {'key': {'participants': '0@s.whatsapp.net', 'fromMe': false, 'id': '3B64558B07848BD81108C1D14712018E'}, 'message': {'locationMessage': {'name': `${user}`, 'jpegThumbnail': await (await fetch(pp)).buffer(), 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'}
+
+let picture = './media/menus/img5.jpg'
+let myling = `
+┏━━⪼ 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 ≪━━◉
+┃
+┃ *${usedPrefix}bin (6 DIGITOS)*
+┃ *EJEMPLO: ${usedPrefix}bin 759385xxxx*
+┃
+┃
+┃
+┃
+┃
+┃
+┃
+┃
+┃
+`
+await conn.sendFile(m.chat, picture, 'Hola.mp3', myling, fkontak)}
+
+handler.command = /^(generarbin|generarbins|genericbin|genericbins|generatebins|binshacking|bins)/i
+export default handler
