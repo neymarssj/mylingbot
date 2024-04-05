a27ce19cc9eeba577a09b6eb3fefd2df4bb18e5d
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-04-05 16:07:00 -0300

    Update generic-bins.js

--- plugins/generic-bins.js
+++ plugins/generic-bins.js
@@ -1,33 +1,23 @@
-let handler = async(m, { conn, text, usedPrefix, command }) => {
-let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let name = conn.getName(who)
-const pp = './storage/logos/Menu2.jpg'
-//const l = {'key': {'participants': '0@s.whatsapp.net', 'fromMe': false, 'id': '3B64558B07848BD81108C1D14712018E'}, 'message': {'locationMessage': {'name': `彡 𝐌𝐨𝐝𝐞𝐫𝐦𝐞 𝐁𝐲 𝐖𝐢𝐥𝐬𝐨𝐧 - 𝐖𝐚𝐨𝐬`, `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'}
+let handler  = async (m, { conn, usedPrefix, command }) => {
+const fkontak = {'key': {'participants': '0@s.whatsapp.net', 'fromMe': false, 'id': '3B64558B07848BD81108C1D14712018E'}, 'message': {'locationMessage': {'name': `彡 𝐌𝐨𝐝𝐞𝐫𝐦𝐞 𝐁𝐲 𝐆𝐋 𝐘𝐓 𝐌𝐗`, 'jpegThumbnail': await (await fetch(instalacion)).buffer(), 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'}
 
-let bins = `┏━━ 𝖢𝖮𝖬𝖠𝖭𝖣𝖮𝖲 ━━⪩
+let picture = './media/menus/img5.jpg'
+let myling = `
+┏━━⪼ 𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 ≪━━◉
 ┃
-┃ 🌹 ${usedPrefix}bin *(6 DIGITOS)*
-┃ 🌹 *EJEMPLO:* ${usedPrefix}bin 759385xxxx
-┃
-┃ 🌹 ${usedPrefix}gen *(6-8 DIGITOS)*
-┃ 🌹 *EJEMPLO:* ${usedPrefix}gen 74792185xxxx
-┃
+┃ *${usedPrefix}bin (6 DIGITOS)*
+┃ *EJEMPLO: ${usedPrefix}bin 759385xxxx*
 ┃
 ┃
 ┃
 ┃
 ┃
-┗━━━━━━━━━━━⪩`
+┃
+┃
+┃
+┃
+`
+await conn.sendFile(m.chat, picture, 'Hola.mp3', myling, fkontak)}
 
-conn.sendMessage(m.chat, {text: bins, mentions: [m.sender]}, {quoted: m})
-
-//conn.reply(m.chat, `*🚩 Ocurrió un fallo*`)
-
-}
-handler.help = ['love']
-handler.tags = ['juegos']
-handler.command = /^(bins|bin|byns)$/i
-
-handler.register = true
-
+handler.command = /^(generarbin|generarbins|genericbin|genericbins|generatebins|binshacking|bins)/i
 export default handler
